68426649368de0b5edb47dbd251071a04d10c447
flyingorangejam <flyingorangejam@gmail.com>
2025-01-26 16:51:38 -0800

    Add particle
    now there are collision effect(particle) when player hits board

--- Assets/Scripts/StickableObject.cs
+++ Assets/Scripts/StickableObject.cs
@@ -1,10 +1,28 @@
-// StickableObject.cs
 using UnityEngine;
 
 public class StickableObject : MonoBehaviour
 {
+    [SerializeField] private GameObject particlePrefab; // Define particle prefab
+
     void Start()
     {
         gameObject.tag = "Stickable";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision detected with: " + other.gameObject.name);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player collision confirmed");
+            if (particlePrefab != null)
+            {
+                Instantiate(particlePrefab, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Particle prefab is null");
+            }
+        }
     }
 }
